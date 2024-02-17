e81aba93faf28702b37e4178a342651b3d33e5ef
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-02-17 22:55:32 +0900

    Fix:next-intl middleware 및 i18n 관련 설정 수정 (#3)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,13 @@
-import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-const locales = ['kr', 'en'];
+const locales = ['ko', 'en-US'];
 
 export default getRequestConfig(async ({ locale }) => {
-    if (!locales.includes(locale)) notFound();
+    if (!locales.includes(locale) || locale === 'en-US') {
+        return {
+            messages: (await import(`../messages/en-US.json`)).default,
+        };
+    }
     return {
         messages: (await import(`../messages/kr.json`)).default,
     };
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,16 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
     // A list of all locales that are supported
-    locales: ['kr', 'en'],
+    locales: ['ko', 'en-US'],
 
     // Used when no locale matches
-    defaultLocale: 'kr',
+    defaultLocale: 'ko',
+    alternateLinks: true,
+    localeDetection: true,
+    localePrefix: 'never',
 });
 
 export const config = {
     // Match only internationalized pathnames
-    matcher: ['/:path*', '/kr/:path*'],
+    matcher: ['/', '/(ko|en-US)/:path*'],
 };
