dcb5b81c2d4956814d1470a0a12b00f19b02752c
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-02-22 13:18:40 +0900

    Feat: locale 영문 테스트 추가 (#6)

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -5,6 +5,7 @@ import { AbstractIntlMessages, NextIntlClientProvider } from 'next-intl';
 
 describe('홈화면 랜더링 테스트', () => {
     let messages: AbstractIntlMessages;
+
     beforeAll(async () => {
         messages = await import('../messages/ko.json');
     });
@@ -15,6 +16,17 @@ describe('홈화면 랜더링 테스트', () => {
             </NextIntlClientProvider>,
         );
         const h1Text = screen.getByText('메타 인지 테스트');
+        expect(h1Text).toBeInTheDocument();
+    });
+    it('영문 로케일 테스트 locale en 시 영문 텍스트 출력 여부', async () => {
+        const enMessages = await import('../messages/en.json');
+
+        render(
+            <NextIntlClientProvider locale="en" messages={enMessages}>
+                <Index />
+            </NextIntlClientProvider>,
+        );
+        const h1Text = screen.getByText('meta-test');
         expect(h1Text).toBeInTheDocument();
     });
 });
