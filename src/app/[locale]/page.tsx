4e85ab77565dadfe9e8dcd10c58ef19915f1d4ab
minchodang <minsu910725@gmail.com>
2024-02-22 14:00:43 +0900

    Feat: 기본 디폴트 버튼 추가 및 전체 레이아웃 설정

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,17 @@
+import Button from '@src/components/common/Button';
 import { useTranslations } from 'next-intl';
 
 const Index = () => {
     const t = useTranslations('Index');
-    return <h1 className="text-3xl font-bold underline">{t('title')}</h1>;
+
+    return (
+        <div className="md:container md:mx-auto">
+            <h1 className="text-3xl font-bold underline">{t('title')}</h1>
+            <Button type="button" classes="bg-stone-600 text-black hover:text-black">
+                테스트용도
+            </Button>
+        </div>
+    );
 };
 
 export default Index;
