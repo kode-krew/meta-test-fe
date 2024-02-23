cfe090792d381175c8e8c8b66702380b6565ebfb
minchodang <minsu910725@gmail.com>
2024-02-23 12:58:44 +0900

    추가: docker-file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+};
 
 module.exports = withNextIntl(nextConfig);
