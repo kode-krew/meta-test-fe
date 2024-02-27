920fb05370b29d393a864350c9af14b88b1aae27
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-02-27 20:57:57 +0900

    Feat: 기본 모달 생성 (#11)

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,34 @@
+'use client';
+
+import { useState } from 'react';
 import Button from '@src/components/common/Button';
+import Modal from '@src/components/common/Modal';
 import HomeFooterButton from '@src/components/screens/home/components/HomeFooterButton';
 import HomeBody from '@src/components/screens/home/section/HomeBody';
 import HomeFooter from '@src/components/screens/home/section/HomeFooter';
 import HomeHeader from '@src/components/screens/home/section/HomeHeader';
+import { AppProps } from 'next/app';
 import { useTranslations } from 'next-intl';
 
-const Index = () => {
-    const t = useTranslations('Index');
+const Index = ({ Component }: AppProps) => {
+    // console.log(Component);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleModal = () => {
+        setIsOpen((prev) => !prev);
+    };
 
     return (
         <div className="w-screen">
-            <HomeHeader />
+            <HomeHeader onClickLoginButton={handleModal} />
             <HomeBody />
             <HomeFooter />
             <HomeFooterButton />
+            {isOpen && (
+                <Modal onClose={handleModal}>
+                    <div>모달처리! </div>
+                </Modal>
+            )}
         </div>
     );
 };
--- src/components/screens/home/section/HomeHeader.tsx
+++ src/components/screens/home/section/HomeHeader.tsx
@@ -1,11 +1,15 @@
-import { FC } from 'react';
+import { FC, MouseEventHandler } from 'react';
 import Button from '@src/components/common/Button';
 
-interface HomeHeaderProps {}
+interface HomeHeaderProps {
+    onClickLoginButton: MouseEventHandler<HTMLButtonElement>;
+}
 
-const HomeHeader: FC<HomeHeaderProps> = () => (
+const HomeHeader: FC<HomeHeaderProps> = ({ onClickLoginButton }) => (
     <header className="m-5 flex flex-row-reverse">
-        <Button variant="secondary">로그인 / 회원가입</Button>
+        <Button onClick={onClickLoginButton} variant="secondary">
+            로그인 / 회원가입
+        </Button>
     </header>
 );
 
