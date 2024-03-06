d6717ea1f3a1a1507c441ae54cd555cd003d8d1f
minchodang <minsu910725@gmail.com>
2024-03-06 17:39:06 +0900

    Feat: 비밀번호 찾기 모달 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,32 +1,39 @@
 import { FC, PropsWithChildren, ReactNode } from 'react';
+import Portal from './Portal';
 
 interface ModalProps {
     children: ReactNode;
     onClose: () => void; // 모달을 닫는 함수를 prop으로 받습니다.
+    isPortal?: boolean;
 }
 
-const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, onClose }) => (
-    <div
-        className="
-      fixed
-      top-0
-      z-40
-      flex h-screen
-      w-screen items-center justify-center
-      bg-black bg-opacity-50" // 배경을 반투명하게 만듭니다.
-        onClick={onClose} // 배경을 클릭하면 모달이 닫히도록 합니다.
-    >
+const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, onClose, isPortal }) => {
+    const modal = (
         <div
             className="
-        w-3/6
-        rounded-lg
-        bg-white
-        p-0" // 모달 창을 스타일링합니다.
-            onClick={(e) => e.stopPropagation()} // 모달 창을 클릭했을 때는 모달이 닫히지 않도록 합니다.
+            fixed
+            left-0
+            top-0
+            z-40
+            flex h-screen
+            w-screen items-center justify-center
+            bg-black bg-opacity-50"
+            onClick={onClose}
         >
-            {children}
+            <div
+                className="
+                w-3/6
+                rounded-lg
+                bg-white
+                p-0"
+                onClick={(e) => e.stopPropagation()}
+            >
+                {children}
+            </div>
         </div>
-    </div>
-);
+    );
+
+    return modal;
+};
 
 export default Modal;
--- src/components/screens/home/components/HomeBasicLoginSection.tsx
+++ src/components/screens/home/components/HomeBasicLoginSection.tsx
@@ -1,31 +1,68 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Button from '@src/components/common/Button';
 import CommonInput from '@src/components/common/CommonInput';
+import Modal from '@src/components/common/Modal';
+import HomePasswordFindScreen from './HomePasswordFindScreen';
 
 interface HomeBasicLoginSectionProps {}
+interface OpenModalState {
+    isOpenSignupModal: boolean;
+    isOpenPasswordModal: boolean;
+}
 
-const HomeBasicLoginSection: FC<HomeBasicLoginSectionProps> = () => (
-    <form className="container mt-4">
-        <div>
-            <label htmlFor="email" className="font-semibold">
-                Email
-            </label>
-            <CommonInput type="email" id="email" placeholder="이메일을 입력해주세요." />
-        </div>
-        <div className="mt-3">
-            <label htmlFor="password" className="font-semibold">
-                Password
-            </label>
-            <CommonInput id="password" type="password" placeholder="비밀번호를 입력해주세요." />
-        </div>
-        <div className="mt-6 flex flex-col gap-3">
-            <Button variant="primary-unselect">이메일 회원가입</Button>
-            <Button variant="primary">이메일 회원가입</Button>
-            <span className="flex w-full justify-end text-sm font-medium text-gray-500">
-                비밀번호를 잊으셨나요?
-            </span>
-        </div>
-    </form>
-);
+const HomeBasicLoginSection: FC<HomeBasicLoginSectionProps> = () => {
+    const [isOpenModal, setIsOpenModal] = useState<OpenModalState>({
+        isOpenPasswordModal: false,
+        isOpenSignupModal: false,
+    });
+
+    const onClickPasswordFind = () => {
+        setIsOpenModal((prev) => ({ ...prev, isOpenPasswordModal: !prev.isOpenPasswordModal }));
+    };
+
+    const onCloseModal = () => {
+        setIsOpenModal((prev) => ({
+            isOpenSignupModal: !prev.isOpenSignupModal,
+            isOpenPasswordModal: !prev.isOpenPasswordModal,
+        }));
+    };
+    return (
+        <>
+            <form className="container mt-4">
+                <div>
+                    <label htmlFor="email" className="font-semibold">
+                        Email
+                    </label>
+                    <CommonInput type="email" id="email" placeholder="이메일을 입력해주세요." />
+                </div>
+                <div className="mt-3">
+                    <label htmlFor="password" className="font-semibold">
+                        Password
+                    </label>
+                    <CommonInput
+                        id="password"
+                        type="password"
+                        placeholder="비밀번호를 입력해주세요."
+                    />
+                </div>
+                <div className="mt-6 flex flex-col gap-3">
+                    <Button variant="primary-unselect">이메일 회원가입</Button>
+                    <Button variant="primary">로그인</Button>
+                    <span
+                        className="flex w-full cursor-pointer justify-end text-sm font-medium text-gray-500"
+                        onClick={onClickPasswordFind}
+                    >
+                        비밀번호를 잊으셨나요?
+                    </span>
+                </div>
+            </form>
+            {isOpenModal.isOpenPasswordModal && (
+                <Modal onClose={onCloseModal}>
+                    <HomePasswordFindScreen />
+                </Modal>
+            )}
+        </>
+    );
+};
 
 export default HomeBasicLoginSection;
