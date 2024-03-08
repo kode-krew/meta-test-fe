c1573dc41cf2d396dc42dbb77018d2da317fc2ed
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-03-08 13:31:08 +0900

    Feat: 홈 회원가입 모달 추가 (#18)

---
+++ src/components/screens/home/components/HomeSignupScreen.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import Button from '@src/components/common/Button';
+import CommonInput from '@src/components/common/CommonInput';
+
+interface HomeSignupScreenProps {}
+
+const HomeSignupScreen: FC<HomeSignupScreenProps> = () => (
+    <div className="container rounded-md border border-violet-400 bg-violet-200 px-5 pb-5 pt-7">
+        <p className="text-2xl font-bold">회원가입</p>
+        <div className="mt-5 flex flex-col gap-3">
+            <CommonInput placeholder="아이디(이메일)" />
+            <CommonInput placeholder="비밀번호" />
+            <CommonInput placeholder="비밀번호 확인" />
+        </div>
+        <div className="mt-5">
+            <Button variant="primary">완료</Button>
+        </div>
+    </div>
+);
+
+export default HomeSignupScreen;
