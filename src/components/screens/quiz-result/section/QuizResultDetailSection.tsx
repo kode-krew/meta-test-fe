63888290837163e46b309eb73d752425f8f42470
minchodang <minsu910725@gmail.com>
2024-03-16 14:58:45 +0900

    Fix: 홈 회원가입 인증버튼 추가 및 공컴 variable 추가

---
+++ src/components/screens/quiz-result/section/QuizResultDetailSection.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface QuizResultDetailSectionProps {}
+
+const QuizResultDetailSection: FC<QuizResultDetailSectionProps> = () => (
+    <div>QuizResultDetailSection</div>
+);
+
+export default QuizResultDetailSection;
