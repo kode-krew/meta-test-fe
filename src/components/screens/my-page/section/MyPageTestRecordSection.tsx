5f823407dac747e84a1f44f4a35d221dcb4621dd
minchodang <minsu910725@gmail.com>
2024-05-04 12:58:05 +0900

    Fix: 랭킹 카드 삭제

--- src/components/screens/my-page/section/MyPageTestRecordSection.tsx
+++ src/components/screens/my-page/section/MyPageTestRecordSection.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import QuizResultHistoryGraphCard from '@src/components/common/quiz-result/QuizResultHistoryGraphCard';
-import QuizResultRankingCard from '@src/components/common/quiz-result/QuizResultRankingCard';
 
 interface MyPageTestRecordSectionProps {
     isLoginSns: boolean;
@@ -10,7 +9,6 @@ const MyPageTestRecordSection: FC<MyPageTestRecordSectionProps> = ({ isLoginSns 
     <section className="flex flex-col">
         <h1 className="text-2xl font-bold">나의 테스트 기록</h1>
         <QuizResultHistoryGraphCard isLoginSns={isLoginSns} />
-        {isLoginSns ? <QuizResultRankingCard /> : null}
     </section>
 );
 
