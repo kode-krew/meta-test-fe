2cafccd91b19ce32276d69ca93a031f203817b2b
minchodang <minsu910725@gmail.com>
2024-05-19 12:27:24 +0900

    Fix: env 업데이트 및 https policy meta 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import GlobalModal from '@src/components/common/modal/GlobalModal';
 import Toaster from '@src/components/common/toast/Toast';
 import './globals.css';
 import TanstackQueryProvider from '@src/provider/TanstackQueryProvider';
+import Head from 'next/head';
 
 interface LocaleLayoutProps {
     children: React.ReactNode;
@@ -10,6 +11,9 @@ interface LocaleLayoutProps {
 
 const LocaleLayout: FC<LocaleLayoutProps> = ({ children }) => (
     <html lang="ko">
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
         <body>
             <TanstackQueryProvider>
                 {children}
