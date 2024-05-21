860c0a041cf667872ec6a0e085531edec1ca3422
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-05-21 12:30:52 +0900

    Feat: seo tag 생성 (#63)
    * Fix: build error 및 빌드 스크립트 구축 (#60)
    
    * Fix: graph recharts로 수정 및 데이터 없을 때 처리
    
    * Fix: bulid-error 및 쉘 스크립트 작성
    
    * Fix: env 업데이트 및 https policy meta 태그 추가
    
    * Fix: auth page meta 추가
    
    * Fix: meta-tag 재수정
    
    * Fix: meta-tag 재수정 2차
    
    * Fix: 불필요한 메타태그 제거
    
    * Feat: seo tag 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,39 @@ import GlobalModal from '@src/components/common/modal/GlobalModal';
 import Toaster from '@src/components/common/toast/Toast';
 import './globals.css';
 import TanstackQueryProvider from '@src/provider/TanstackQueryProvider';
-import Head from 'next/head';
 import { Metadata } from 'next';
 
 interface LocaleLayoutProps {
     children: React.ReactNode;
 }
 
+export const metadata: Metadata = {
+    title: '메타인지 테스트',
+    description: '메타인지를 테스트 할 수 있는 사이트입니다.',
+    openGraph: {
+        type: 'website',
+        siteName: 'meta-cognition-test',
+        locale: 'ko-kr',
+        title: '메타인지 테스트',
+        images: ['/home/meta-test-main.png'],
+        description: '당신의 메타인지를 테스트 해 보세요.',
+    },
+    twitter: {
+        title: '메타인지 테스트',
+        description: '당신의 메타인지를 테스트 해 보세요.',
+        images: ['/home/meta-test-main.png'],
+    },
+};
+
 const LocaleLayout: FC<LocaleLayoutProps> = ({ children }) => (
     <html lang="ko">
+        <head>
+            <link rel="icon" href="meta-favicon.png" sizes="any" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="keywords" content="메타인지, 테스트, 인지능력, 자기인식" />
+            <meta name="robots" content="index, follow" />
+            <meta name="author" content="kode-krew" />
+        </head>
         <body>
             <TanstackQueryProvider>
                 {children}
