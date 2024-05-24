f2842e4668bc4c38830e04ea8ec52df2267070c1
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-05-24 09:54:23 +0900

    Feat: sitemap google search console script (#70)

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.meta-cognition.site',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize:7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
+
