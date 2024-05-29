a3494aabff3310b88829d9fe9562a28f0efb5936
minchodang <minsu910725@gmail.com>
2024-05-29 11:37:48 +0900

    Feat: 공유하기 기능 및 prettier , eslint 설정

--- src/components/screens/quiz-result/QuizResultFooter.tsx
+++ src/components/screens/quiz-result/QuizResultFooter.tsx
@@ -4,21 +4,33 @@ import { FC } from 'react';
 import RefreshIcon from '@src/components/common/Icons/RefreshIcon';
 import ShareIcon from '@src/components/common/Icons/ShareIcon';
 import { useRouter } from 'next/navigation';
+import { ToastService } from '@src/service/ToastService';
 
 interface QuizResultFooterProps {}
 
 const QuizResultFooter: FC<QuizResultFooterProps> = () => {
+    const toastService = ToastService.getInstance();
     const { replace } = useRouter();
 
-    const onClickRefreshIcon = () => {
-        replace('/');
+    const onClickShareIcon = () => {
+        navigator
+            .share({
+                title: '메타 인지 테스트',
+                text: '메타 인지를 테스트 해 보세요.',
+                url: 'https://meta-cognition.site/',
+            })
+            .then(() => {
+                toastService.addToast('공유가 완료되었습니다.');
+            })
+            .catch(() => {
+                toastService.addToast('공유가 실패했습니다. 잠시후 다시 시도해 주세요.');
+            });
     };
-    const onClickShareIcon = () => {};
 
     return (
         <footer className="fixed bottom-4 left-0 flex w-full justify-between">
             <div
-                onClick={onClickRefreshIcon}
+                onClick={() => replace('/')}
                 className="container flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-violet-400"
             >
                 <RefreshIcon />
