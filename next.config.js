8c0126e068257abd7baac63bddb6c6e60b2bc2ab
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-06-01 12:26:41 +0900

    Feat: sentry setting (#75)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
+const { withSentryConfig } = require('@sentry/nextjs');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
     output: 'standalone',
+    sentry: {
+        hideSourceMaps: true,
+    },
 };
 
-module.exports = withNextIntl(nextConfig);
+const sentryWebpackConfig = {
+    silent: true,
+    authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
+};
+module.exports = withNextIntl(withSentryConfig(nextConfig, sentryWebpackConfig));
