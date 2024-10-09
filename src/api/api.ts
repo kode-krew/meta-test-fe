d2033cfd22cd6b0a286edaa2d2c6839f9321753a
minchodang <minsu910725@gmail.com>
2024-10-09 09:31:10 +0900

    Fix: oas에 맞춰 auth-provider 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,11 +2,8 @@ import type { paths } from '@src/__generated__/api';
 import createFetchClient from 'openapi-fetch';
 import createClient from 'openapi-react-query';
 
-const client = createFetchClient<paths>({
+export const client = createFetchClient<paths>({
     baseUrl: `${process.env.NEXT_PUBLIC_META_TEST_SERVER_HOST_URL}`,
-    headers: {
-        authorization:``
-    }
 });
 
 export const $api = createClient(client);
